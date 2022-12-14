53d9d8fed957e2e82223328cdc969df9e511f560
kondiamantis <kondiamantis@gmail.com>
2022-12-14 20:24:06 +0200

    Added flyable runner

---
+++ src/FlyableRunner.java
@@ -0,0 +1,33 @@
+public class FlyableRunner {
+
+
+    public static void main(String[] args) {
+        Flyable[] flyingObjects = {new Bird(), new Aeroplane()};
+
+        for(Flyable object: flyingObjects){
+            object.fly();
+        }
+
+    }
+}
+
+interface Flyable{
+
+    void fly();
+}
+
+class Bird implements Flyable{
+
+    @Override
+    public void fly() {
+        System.out.println("With wings");
+    }
+}
+
+class Aeroplane implements Flyable{
+
+    @Override
+    public void fly() {
+        System.out.println("With fuel");
+    }
+}
